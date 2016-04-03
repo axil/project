d25c599d784d38373c2c97afb1df850ffcd7495c
Alximick <alximick@gmail.com>
2016-04-03 09:51:32 +0600

    filter notes with username

--- notes/views.py
+++ notes/views.py
@@ -12,14 +12,16 @@ def hello(request):
 
 def notes(request):
     args = {}
-    args['notes'] = Notes.objects.order_by('-date')
+
     args['username'] = auth.get_user(request).username
+    print(args['username'])
+    args['notes'] = Notes.objects.filter(author=auth.get_user(request).id)
     args['projects'] = Category.objects.all()
     return render_to_response('notes.html', args)
 
 def note(request, id=None):
     args = {}
-    args['note'] = Notes.objects.get(id=id)
+    args['note'] = Notes.objects.get(id=id, author=auth.get_user(request).id)
     args['username'] = auth.get_user(request).username
     args['projects'] = Category.objects.all()
     return render_to_response('note.html', args)
@@ -30,7 +32,7 @@ def category(request, category_id=1):
     args = {}
     args['projects'] = Category.objects.all()
     args['category'] = Category.objects.get(id=category_id)
-    args['notes'] = Notes.objects.filter(category=category_id)
+    args['notes'] = Notes.objects.filter(category=category_id, author=auth.get_user(request).id)
     args['username'] = auth.get_user(request).username
     # branch_categories = args['category'].get_descendants(include_self=True)
     # args['category_articles'] = Notes.objects.filter(category__in=branch_categories).distinct()
