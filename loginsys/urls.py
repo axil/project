05d1a73d9d58dd947afd8712edcdb57860f2ceb1
Alximick <alximick@gmail.com>
2016-04-03 17:45:23 +0600

    {% url 'names' %}

--- loginsys/urls.py
+++ loginsys/urls.py
@@ -3,8 +3,8 @@ import loginsys.views
 
 urlpatterns = [
 
-    url(r'^login/$', loginsys.views.login),
-    url(r'^logout/$', loginsys.views.logout),
-    url(r'^register/$', loginsys.views.register),
+    url(r'^login/$', loginsys.views.login, name='login'),
+    url(r'^logout/$', loginsys.views.logout, name='logout'),
+    url(r'^register/$', loginsys.views.register, name='register'),
     url(r'^$', loginsys.views.login),
 ]
